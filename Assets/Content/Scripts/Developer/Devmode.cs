a3fe6bb4a01902f99dfb4687e156c592f2413d7e
Isaac Nunez <Isaacnunez2002@icloud.com>
2020-03-02 15:09:11 -0500

    Added Devmode code. No Functionallity Yet

---
+++ Assets/Content/Scripts/Developer/Devmode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Arctic.Keybinds;
+
+public class Devmode : MonoBehaviour
+{
+    bool devmode;
+    KeybindingManager Keybinds;
+
+
+    public void Start()
+    {
+        devmode = false;
+        
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(Keybinds._DevMode))
+        {
+            if (devmode)
+            {
+                devmode = false;
+            } else if (!devmode)
+            {
+                //TODO: Add Devmode functions
+            }
+        }
+    }
+}
